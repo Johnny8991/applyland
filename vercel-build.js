874db17a32a20cb93afe4adfb81836fe35367a4a
Johnny8991 <168196164+Johnny8991@users.noreply.github.com>
2026-07-21 10:16:36 +0100

    Delete vercel-build.js

--- vercel-build.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-process.env.KIT_PROJECT_DIR = __dirname
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
-require('govuk-prototype-kit/lib/build').generateAssetsSync()
-
-console.log('Built GOV.UK Prototype Kit assets for Vercel')
